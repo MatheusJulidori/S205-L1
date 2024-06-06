7276d35c4df96ddb771a96039f586bdbfe56c8d1
matheusjulidori <matheus_julidori@yahoo.com.br>
2024-06-05 22:19:48 -0300

    Correção

--- s205/inatel-app/src/views/Disciplina/Disciplina.js
+++ s205/inatel-app/src/views/Disciplina/Disciplina.js
@@ -5,17 +5,17 @@ import './Disciplina.css';
 
 function Disciplina() {
     const Disciplinas = [
-        ["C214", "8/10", "85", "-","13/06"],
-        ["S107", "12/20", "84" , "-","12/06"],
-        ["S205", "5/30", "72", "-","26/06"],
-        ["C216", "0/30", "70","-", "24/06"],
-        ["G008", "12/15", "80","-", "21/06"],
+        ["C214", "8/10", "85", "-"],
+        ["S107", "12/20", "84" , "-"],
+        ["S205", "5/30", "72", "-"],
+        ["C216", "0/30", "70","-"],
+        ["G008", "12/15", "80","-"],
     ];
 
     return (
         <main className="main-content">
             <Header />
-            <Table title="Professores" columns={["Código Disc.", "Frequencia", "NP1", "NP2", "Próxima Avaliação"]} data={Disciplinas} />
+            <Table title="Disciplinas" columns={["Código Disc.", "Frequencia", "NP1", "NP2"]} data={Disciplinas} />
         </main>
     );
 }
